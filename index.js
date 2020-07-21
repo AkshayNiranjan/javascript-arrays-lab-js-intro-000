ecb62e7713a07c6e17b2094fa61c876bc42e5a3c
Akshay Niranjan <anstmfriend@gmail.com>
2020-07-21 09:37:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function removeLastKitten(arr){
-  
+  return arr.slice(0,arr.length-1)
 }
 function removeFirstKitten(arr){
   return arr.slice(1)
